80d48ecf5d9369a6b6a2a494e3899a2176da19eb
Najibu Nsubuga <ns.najibu@gmail.com>
2016-03-15 23:36:33 +0000

    Installed Devise

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistraionsController < Devise::RegistraionsController
+
+	private
+	def sign_up_params
+		params.require(:user).permit(:name, :email, :password)
+	end
+
+	def account_update_params
+		params.require(:user).permit(:name, :email, :password, :current_password)
+	end
+end
